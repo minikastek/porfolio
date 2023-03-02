8d22993beb20cab5ffd8c68dddf8f6f8025db68a
Nico Olivera <oliveran9@gmail.com>
2023-03-02 15:20:08 -0300

    primer commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiTrello,
+  SiJirasoftware,
+  SiIos,
+  SiAndroid,
+  SiFirebase,
+} from "react-icons/si";
+import {
+  DiMongodb,
+} from "react-icons/di";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiIos />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTrello />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJirasoftware />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAndroid />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdfEnglish from "../../Assets/CV-English-Olivera-Nicolas.pdf";
+import pdfSpanish from "../../Assets/CV-Español-Olivera-Nicolas.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink =
+  "https://raw.githubusercontent.com/minikastek/portfolio/master/src/Assets/CV English Olivera Nicolas.pdf";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Col>
+            <Button
+              variant="primary"
+              href={pdfEnglish}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV in English
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              href={pdfSpanish}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Descargar CV en Español
+            </Button>
+          </Col>
+        </Row>
+
+        <Row className="resume">
+          <Document file={resumeLink} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Col>
+            <Button
+              variant="primary"
+              href={pdfEnglish}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV in English
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="primary"
+              href={pdfSpanish}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Descargar CV en Español
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
